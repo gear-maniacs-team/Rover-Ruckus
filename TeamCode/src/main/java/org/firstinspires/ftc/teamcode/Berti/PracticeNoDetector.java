b13a56f5476b517221bd35f16366a00c8ad44a39
Olareanu Alexandru <alexandruola@gmail.com>
2019-01-25 22:31:29 +0200

    Corrected NullException

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Berti/PracticeNoDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Berti/PracticeNoDetector.java
@@ -2,18 +2,22 @@ package org.firstinspires.ftc.teamcode.Berti;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 import org.firstinspires.ftc.teamcode.motors.WheelMotors;
 
 @Autonomous(name = "Practice Encoders")
 public class PracticeNoDetector extends LinearOpMode
 {
-    WheelMotors wheelMotors = new WheelMotors(hardwareMap.dcMotor);
+    private WheelMotors wheelMotors = null;
     private double speed = 0.4;
 
     @Override
     public void runOpMode() throws InterruptedException
     {
+        wheelMotors = new WheelMotors(hardwareMap.dcMotor);
         telemetry.addData("Status", "Ready to run");
         telemetry.update();
 
@@ -34,9 +38,9 @@ public class PracticeNoDetector extends LinearOpMode
     private void goFront(double setedSpeed)
     {
         wheelMotors.TR.setPower(setedSpeed);
-        wheelMotors.TL.setPower(setedSpeed);
+        wheelMotors.TL.setPower(-setedSpeed);
         wheelMotors.BR.setPower(setedSpeed);
-        wheelMotors.BL.setPower(setedSpeed);
+        wheelMotors.BL.setPower(-setedSpeed);
 
         sleep(1000);
 
@@ -44,17 +48,21 @@ public class PracticeNoDetector extends LinearOpMode
         wheelMotors.TL.setPower(0);
         wheelMotors.BR.setPower(0);
         wheelMotors.BL.setPower(0);
+
+        sleep(1000);
     }
 
     private void goLeft(double setedSpeed)
     {
         wheelMotors.TR.setPower(setedSpeed);
-        wheelMotors.TL.setPower(-setedSpeed);
+        wheelMotors.TL.setPower(setedSpeed);
         wheelMotors.BR.setPower(setedSpeed);
-        wheelMotors.BL.setPower(-setedSpeed);
+        wheelMotors.BL.setPower(setedSpeed);
 
         sleep(1000);
 
         goFront(setedSpeed);
+
+        sleep(1000);
     }
 }
