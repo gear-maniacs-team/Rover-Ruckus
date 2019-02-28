75b8c5ae063f305429ec0e7c7998a97d3938f62a
Berti <berti_florea@yahoo.com>
2019-02-28 10:54:59 +0200

    Added Strafe (left - right)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalTeleOp.java
@@ -11,11 +11,6 @@ public class FinalTeleOp extends OpMode {
     private static final double PRECISION_MODE_MULTIPLIER = 0.55;
     private static final double MOTOR_SPEED_MULTIPLIER = 0.9;
     private static final double MOTOR_SPEED_ROTATION = 0.3;
-    private double ptl;
-    private double ptr;
-    private double pbl;
-    private double pbr;
-    private double max;
 
     private WheelMotors wheelMotors = null;
     private boolean precisionModeOn;
@@ -30,8 +25,15 @@ public class FinalTeleOp extends OpMode {
     @Override
     public void loop()
     {
+        Movement();
+        Strafe();
+    }
+
+    public void Movement()
+    {
         final double leftX = gamepad1.left_stick_x;
         final double leftY = -gamepad1.left_stick_y;
+        double ptl, ptr, pbl, pbr, max;
 
         ptl = leftY + leftX;
         pbl = leftY + leftX;
@@ -42,11 +44,11 @@ public class FinalTeleOp extends OpMode {
         if (max < pbl)
             max = pbl;
         else
-            if (max < ptr)
-                max = ptr;
-            else
-                if (max < pbr)
-                    max = pbr;
+        if (max < ptr)
+            max = ptr;
+        else
+        if (max < pbr)
+            max = pbr;
         if (max > 1)
         {
             ptl /= max;
@@ -59,5 +61,15 @@ public class FinalTeleOp extends OpMode {
         wheelMotors.TL.setPower(ptl);
         wheelMotors.BR.setPower(pbr);
         wheelMotors.BL.setPower(pbl);
+    }
+
+    public void Strafe()
+    {
+        final double rightX = gamepad1.right_stick_x;
+
+        wheelMotors.TR.setPower(rightX);
+        wheelMotors.TL.setPower(rightX);
+        wheelMotors.BR.setPower(-rightX);
+        wheelMotors.BL.setPower(-rightX);
     }
 }
